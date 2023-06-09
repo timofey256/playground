937c7795ff0d31440d2b94adcadb5b91e519d2d3
schetilt <shetilin.timofey@gmail.com>
2023-06-09 22:15:40 +0200

    Played with delegates

--- essential_csharp/delegates/MyDelegatesExamples/Program.cs
+++ essential_csharp/delegates/MyDelegatesExamples/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyDelegatesExamples;
+
+class Delegates
+{
+	private delegate void SayStuff(string str);
+	
+	private static void SayingHelloStuff(string str)
+	{
+		Console.WriteLine($"Hey! {str}");
+	}
+	
+	private static void SayingByeStuff(string str)
+	{
+		Console.WriteLine($"{str}. Bye!");
+	}
+	
+	public static void Main(string[] args)
+	{
+		SayStuff sayStuff = new SayStuff(SayingByeStuff);
+		sayStuff("I am 30 y.o");
+		
+		sayStuff = new SayStuff(SayingByeStuff);
+		sayStuff("I am 30 y.o");
+
+		sayStuff += SayingHelloStuff;
+		sayStuff("Sky is blue");
+	}
+}
