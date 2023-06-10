4e69fcd630da9a6e32e52c24f9c67e69c80fe0b7
schetilt <shetilin.timofey@gmail.com>
2023-06-11 00:52:15 +0200

    Program.cs

--- essential_csharp/inheritance/MyInheritanceExamples/Program.cs
+++ essential_csharp/inheritance/MyInheritanceExamples/Program.cs
@@ -25,9 +25,15 @@ class Czech : Person
 {
 	public Czech(string name) : base(name) {}
 
-	public override void SayHello()
+	public new void SayHello()
 	{
 		Console.WriteLine($"Ahoj, jsem {_name}");
+	}
+
+	public void SayHello2()
+	{
+		SayHello();
+		SayHello();
 	}
 	
 	public override void SayBye()
@@ -36,53 +42,11 @@ class Czech : Person
 	}
 }
 
-class German : Person
-{
-	public German(string name) : base(name) {}
-
-	public override void SayHello()
-	{
-		Console.WriteLine($"Hallo, ich bin {_name}");
-	}
-
-	public new void SayBye()
-	{
-		Console.WriteLine("Tschuss");
-	}
-}
-
 class MainClass
 {
 	public static void Main(string[] args)
 	{
-		Person[] people = new Person[] { new Czech("Martin"), new German("Wolfgang")};
-
-		// How to use virtual functions for polymorfism:
-		foreach(Person p in people)
-		{
-			p.SayHello();
-			p.SayBye();
-		}
-		
-		// `new` and `override` keywords here work the same:
 		Czech p1 = new Czech("Martin");
-		p1.SayBye();
-		German p2 = new German("Wolfgang");
-		p2.SayBye();
-		
-		/* Difference between `new` and `override` keywords.
-		 * 
-		 * Output:
-		 * ----
-		 * Čau
-		 * Bye from person
-		 * ----
-		 * `override` keyword overrides functions even in the parent class, whereas `new` doesn't do it. 
-		 * 
-		 * */
-		Person p1_Czech = (Person)p1;
-		p1_Czech.SayBye();
-		Person p2_German = (Person)p2;
-		p2_German.SayBye();
+		p1.SayHello2();	
 	}
 }
